99dfa9415180e03a4cf1b4280bb1046535fddae5
Famersan <salomoazerpattiwaellapia@gmail.com>
2026-06-02 09:33:54 +0700

    fix CORS middleware placement

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     health: '/up',
 )
     ->withMiddleware(function (Middleware $middleware) {
+    $middleware->use([
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ]);
     $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
-$middleware->use([
-        \Illuminate\Http\Middleware\HandleCors::class,
     ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
