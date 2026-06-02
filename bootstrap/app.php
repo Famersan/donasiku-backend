0cdfdb4036d4a09e353bc5973373722b42ddc0ae
Famersan <salomoazerpattiwaellapia@gmail.com>
2026-06-02 09:30:16 +0700

    add CORS middleware Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ]);
+$middleware->use([
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
